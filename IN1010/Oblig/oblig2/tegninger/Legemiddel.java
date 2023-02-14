955e2da9b2a9ad3a6703e54469e75b1ad5e06602
markusevanger <123594081+markusevanger@users.noreply.github.com>
2023-02-14 20:01:34 +0000

    deleted:    IN1010/Oblig/._oblig0 	deleted:    IN1010/Oblig/._oblig1 	new file:   IN1010/Oblig/oblig2/tegninger/A1.ai 	new file:   IN1010/Oblig/oblig2/tegninger/A1.jpg 	new file:   IN1010/Oblig/oblig2/tegninger/Legemiddel.java

---
+++ IN1010/Oblig/oblig2/tegninger/Legemiddel.java
@@ -0,0 +1,17 @@
+class Legemiddel {
+
+    public String navn;
+    public int pris;
+    public double virkestoff; 
+
+
+
+    public Legemiddel(String navn, int pris, double virkestoff) {
+
+        this.navn = navn;
+        this.pris = pris;
+        this.virkestoff = virkestoff;
+
+
+    }
+}
